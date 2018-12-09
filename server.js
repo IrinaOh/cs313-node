c86da9978f1eccb7965c4b89f82ed1aa168b90c3
Irina O'Hara <irina.v.ohara@gmail.com>
2018-12-09 15:31:12 -0700

    app.use fix

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
-const eventController = require("./controllers/eventController.js");
+// const eventController = require("./controllers/eventController.js");
 const PORT  = process.env.PORT || 5000;
 
-var app = express;
+var app = express();
 
-app.use(express.static(path.join(__dirname + "public")));
-app.use(express.json()); //support json encoded bodies
-app.use(express.urlencoded({extended: true})) //support url encoded bodies
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.json()); //support json encoded bodies
+// app.use(express.urlencoded({extended: true})) //support url encoded bodies
 
-app.get("/events", eventController.getEventList);
+// app.get("/events", eventController.getEventList);
 
-app.get("/event", eventController.getEvent);
+// app.get("/event", eventController.getEvent);
 
-app.post("/event", eventController.postEvent);
+// app.post("/event", eventController.postEvent);
 
 app.listen(PORT, function() {
     console.log("Server listening on port " + PORT);
