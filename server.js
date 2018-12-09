d0277ed37a5cb37d982f2185081f073964c8eb1c
Irina O'Hara <irina.v.ohara@gmail.com>
2018-12-09 16:08:11 -0700

    server.js fix

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const PORT  = process.env.PORT || 5000;
 var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.json()); //support json encoded bodies
-// app.use(express.urlencoded({extended: true})) //support url encoded bodies
+app.use(express.json()); //support json encoded bodies
+app.use(express.urlencoded({extended: true})) //support url encoded bodies
 
-// app.get("/events", eventController.getEventList);
+app.get("/events", eventController.getEventList);
 
-// app.get("/event", eventController.getEvent);
+app.get("/event", eventController.getEvent);
 
-// app.post("/event", eventController.postEvent);
+app.post("/event", eventController.postEvent);
 
 app.listen(PORT, function() {
     console.log("Server listening on port " + PORT);
